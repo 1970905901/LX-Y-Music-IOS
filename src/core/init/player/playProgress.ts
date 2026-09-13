536e4b5612ee3882468cd7c7bd4a82a4b23bd434
1970905901 <1970905901@users.noreply.github.com>
2026-09-14 00:29:33 +0800

    fix: revert playProgress.ts to last known working state (0e165ae)

--- src/core/init/player/playProgress.ts
+++ src/core/init/player/playProgress.ts
@@ -1,9 +1,10 @@
 import { updateListMusics } from '@/core/list'
 import { setMaxplayTime, setNowPlayTime } from '@/core/player/progress'
 import { getTimelineDuration } from '@/core/player/timeline'
-import { setCurrentTime, getDuration, getPosition, isNativeFlacActive } from '@/plugins/player/utils'
+import { setCurrentTime, getDuration, getPosition } from '@/plugins/player/utils'
 import { formatPlayTime2 } from '@/utils/common'
 import { savePlayInfo } from '@/utils/data'
+import { throttleBackgroundTimer } from '@/utils/tools'
 import BackgroundTimer from 'react-native-background-timer'
 import playerState from '@/store/player/state'
 import settingState from '@/store/setting/state'
@@ -113,8 +114,7 @@ export default () => {
       // 直接以该落点锚定 UI 时钟并同步歌词，由每秒 getCurrentTime 校准防止长期漂移。
       audioClock.setAnchor(actualTime * 1000, settingState.setting['player.playbackRate'], playerState.isPlay)
       syncLyric(actualTime, playerState.isPlay)
-      // 统一不在此处启动歌词 ticker：seek 后音频需短暂缓冲才能真正出声，
-      // 由 playing 事件的 play() 以真实音频位置重启 ticker，避免歌词先行。
+      global.app_event.seekLyric(actualTime)
     })
 
     if (maxTime != null) setMaxplayTime(getTimelineDuration(playerState.playMusicInfo.musicInfo, maxTime))

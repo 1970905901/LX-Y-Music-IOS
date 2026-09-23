e3df287d22d540a3102236acaa66522d5f6166a8
1970905901 <1970905901@users.noreply.github.com>
2026-09-23 21:36:30 +0800

    style: use pill search type selector

--- src/screens/Home/Views/Search/SearchTypeSelector.tsx
+++ src/screens/Home/Views/Search/SearchTypeSelector.tsx
@@ -7,7 +7,7 @@ import { useI18n } from '@/lang'
 import Text from '@/components/common/Text'
 import { useTheme } from '@/store/theme/hook'
 import { getSearchSetting } from '@/utils/data'
-import { BorderWidths } from '@/theme'
+import { designRadius, designSpacing } from '@/theme/DesignTokens'
 
 const SEARCH_TYPE_LIST = ['music', 'songlist', 'singer', 'album'] as const
 
@@ -41,23 +41,24 @@ export default () => {
 
   return (
     <ScrollView style={styles.container} keyboardShouldPersistTaps={'always'} horizontal={true}>
-      {list.map((t) => (
+      {list.map((item) => (
         <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            handleTypeChange(t.id)
+          style={{
+            ...styles.button,
+            backgroundColor: type == item.id ? theme['c-primary-background'] : 'rgba(0,0,0,0)',
           }}
-          key={t.id}
+          onPress={() => {
+            handleTypeChange(item.id)
+          }}
+          key={item.id}
         >
           <Text
             style={{
               ...styles.buttonText,
-              borderBottomColor:
-                type == t.id ? theme['c-font-label'] : 'transparent',
+              color: type == item.id ? theme['c-primary-font'] : theme['c-font-label'],
             }}
-            color={type == t.id ? theme['c-font-label'] : theme['c-font']}
           >
-            {t.label}
+            {item.label}
           </Text>
         </TouchableOpacity>
       ))}
@@ -70,29 +71,20 @@ const styles = createStyle({
     height: '100%',
     flexGrow: 0,
     flexShrink: 1,
-    // paddingLeft: 5,
-    // paddingRight: 5,
-    // backgroundColor: 'rgba(0,0,0,0.1)',
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   button: {
-    // height: 38,
-    // lineHeight: 38,
+    height: 34,
     justifyContent: 'center',
-    paddingLeft: 8,
-    paddingRight: 8,
-    // width: 80,
-    // backgroundColor: 'rgba(0,0,0,0.1)',
+    alignItems: 'center',
+    paddingLeft: designSpacing.sm,
+    paddingRight: designSpacing.sm,
+    marginLeft: designSpacing.xs,
+    borderRadius: designRadius.pill,
   },
   buttonText: {
-    // height: 38,
-    // lineHeight: 38,
     textAlign: 'center',
-    paddingLeft: 2,
-    paddingRight: 2,
-    // paddingTop: 10,
-    paddingTop: 3,
-    paddingBottom: 3,
-    borderBottomWidth: BorderWidths.normal3,
-    // width: 80,
+    fontWeight: '600',
   },
 })

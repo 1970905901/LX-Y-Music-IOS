3644ebfb043902a19378e62913a79d156081a91c
1970905901 <1970905901@users.noreply.github.com>
2026-09-23 22:33:23 +0800

    style: modernize about settings

--- src/screens/Home/Views/Setting/settings/About.tsx
+++ src/screens/Home/Views/Setting/settings/About.tsx
@@ -6,6 +6,7 @@ import Section from '../components/Section'
 import { createStyle, openUrl } from '@/utils/tools'
 import { useTheme } from '@/store/theme/hook'
 import { useI18n } from '@/lang'
+import { designRadius, designSpacing, designTypography } from '@/theme/DesignTokens'
 import Text from '@/components/common/Text'
 
 export default memo(() => {
@@ -22,7 +23,7 @@ export default memo(() => {
   }
 
   const textLinkStyle = {
-    ...styles.text,
+    ...styles.link,
     textDecorationLine: 'underline',
     color: theme['c-primary-font'],
   } as const
@@ -30,24 +31,24 @@ export default memo(() => {
   return (
     <Section title={t('setting_about')} sectionId="setting_about">
       <View style={styles.part}>
-        <Text style={styles.text}>本软件(LX-Y Music)完全免费，代码已开源。开源地址：</Text>
+        <Text style={{ ...styles.text, color: theme['c-font'] }}>本软件(LX-Y Music)完全免费，代码已开源。开源地址：</Text>
         <TouchableOpacity onPress={openHomePage}>
           <Text style={textLinkStyle}>https://github.com/1970905901/LX-Y-Music-IOS</Text>
         </TouchableOpacity>
       </View>
       <View style={styles.part}>
-        <Text style={styles.text}>
+        <Text style={{ ...styles.text, color: theme['c-font'] }}>
           由于软件开发的初衷仅是为了对新技术的学习与研究，因此软件直至停止维护都将会一直保持纯净。
         </Text>
       </View>
       <View style={styles.part}>
-        <Text style={styles.text}>
+        <Text style={{ ...styles.text, color: theme['c-font'] }}>
           目前本项目的原始发布地址<Text style={styles.boldText}>只有 GitHub</Text>
           ，其他渠道均为第三方转载发布，可信度请自行鉴别。
         </Text>
       </View>
       <View style={styles.part}>
-        <Text style={styles.text}>
+        <Text style={{ ...styles.text, color: theme['c-font'] }}>
           <Text style={styles.boldText}>
             本项目没有微信公众号之类的所谓「官方账号」，也未在小米、华为、vivo
             等应用商店发布同名应用，谨防被骗！
@@ -55,14 +56,14 @@ export default memo(() => {
         </Text>
       </View>
       <View style={styles.part}>
-        <Text style={styles.text}>
+        <Text style={{ ...styles.text, color: theme['c-font'] }}>
           若你使用过程中遇到<Text style={styles.boldText}>广告</Text>或者
           <Text style={styles.boldText}>引流</Text>
           （如需要加群、关注公众号之类才能使用或者升级）的信息，则表明你当前运行的软件是「第三方修改版」。
         </Text>
       </View>
       <View style={styles.part}>
-        <Text style={styles.text}>
+        <Text style={{ ...styles.text, color: theme['c-font'] }}>
           若在升级新版本时提示「<Text style={styles.boldText}>签名不一致</Text>
           」，则表明你手机上的旧版本或者将要安装的新版本中
           <Text style={styles.boldText}>有一方</Text>是「
@@ -71,7 +72,7 @@ export default memo(() => {
       </View>
       <View style={styles.part}>
         <Text style={styles.boldText}>致谢</Text>
-        <Text style={styles.text}>
+        <Text style={{ ...styles.text, color: theme['c-font'] }}>
           本软件基于以下上游项目构建，在此致以诚挚的感谢：
         </Text>
         <TouchableOpacity onPress={openUpstreamIos}>
@@ -82,13 +83,13 @@ export default memo(() => {
         </TouchableOpacity>
       </View>
       <View style={styles.part}>
-        <Text style={styles.text}>
+        <Text style={{ ...styles.text, color: theme['c-font'] }}>
           QQ交流群：<Text style={styles.boldText}>1013518794</Text>
         </Text>
       </View>
       <View style={styles.part}>
         <Text style={styles.boldText}>赞赏支持</Text>
-        <Text style={styles.text}>
+        <Text style={{ ...styles.text, color: theme['c-font'] }}>
           若你觉得本软件不错，欢迎赞赏支持开发者：
         </Text>
         <Image
@@ -103,33 +104,28 @@ export default memo(() => {
 
 const styles = createStyle({
   part: {
-    marginLeft: 15,
-    marginRight: 15,
-    marginBottom: 10,
+    marginBottom: designSpacing.sm,
     flexDirection: 'row',
     flexWrap: 'wrap',
   },
   text: {
-    fontSize: 14,
-    textAlignVertical: 'bottom',
+    fontSize: designTypography.body,
+    lineHeight: 24,
+  },
+  link: {
+    fontSize: designTypography.body,
+    lineHeight: 24,
   },
   boldText: {
-    fontSize: 14,
+    fontSize: designTypography.body,
     fontWeight: 'bold',
-    textAlignVertical: 'bottom',
-  },
-  throughText: {
-    fontSize: 14,
-    textDecorationLine: 'line-through',
-    textAlignVertical: 'bottom',
-  },
-  btn: {
-    flexDirection: 'row',
+    lineHeight: 24,
   },
   rewardImage: {
-    width: 220,
-    height: 220,
-    marginTop: 10,
+    width: 200,
+    height: 200,
+    marginTop: designSpacing.xs,
+    borderRadius: designRadius.md,
     alignSelf: 'center',
   },
 })
